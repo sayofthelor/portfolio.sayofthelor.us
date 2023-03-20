e6556a8ea79a5ba791e7a34064b416b6086cc549
sayofthelor <35895538+sayofthelor@users.noreply.github.com>
2023-03-20 13:10:52 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -14,10 +14,7 @@ const config = {
 		adapter: adapter({
 			edge: false,
 			split: true
-		}),
-		paths: {
-			base: process.env.NODE_ENV === "production" ? "/research-portfolio" : "",
-		},
+		})
 	}
 };
 
